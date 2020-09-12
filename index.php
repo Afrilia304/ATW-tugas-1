6d4e1b9a6987ef357d8ce8cc4ee0c535fa8fc47c
Afrilia Susanti S <71055187+Afrilia304@users.noreply.github.com>
2020-09-12 12:17:41 -0500

    membuat class hewan

--- index.php
+++ index.php
@@ -1,22 +1,43 @@
 <?php
 
-include "Animal.php";
-
 class Hewan {
-    public $kucing, $anjing, $elang, $angsa, $jumlah_kaki, $nama, $bisa_terbang, $suara;
-
-    public function cetakNama(){
-        return $this->nama;
+    public $jumlahKaki, $bisa_terbang ="Bisa terbang", $bersuara;
     }
-    
-}
 
 class Kucing extends Hewan {
-    public $jumlah_kaki, $nama, $suara;
+    public $bisa_terbang = "Tidak bisa terbang";
 
-    public function cetakNama(){
-        return $this->nama;
+    function jumlahKaki(){
+        return $this->jumlahKaki;
     }
+    
+    public function terbang() {
+        return $this->bisa_terbang;
+    }
+
+    public function bersuara() {
+        return "meong";
+    }
+}
+
+class Anjing extends Hewan {
+    public $bisa_terbang = "Tidak bisa terbang";
+
+    function jumlahKaki() {
+        return $this->jumlahKaki;
+    }
+
+    public function terbang() {
+        return $this->bisa_terbang;
+    }
+
+    public function bersuara(){
+        return "guk guk";
+    }
+}
+
+class Elang extends Hewan {
+    
 }
 
 $momo = new Kucing;
